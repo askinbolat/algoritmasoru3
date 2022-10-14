f1851a26c0dcb9e74a2b6500211886b2ff9ca71e
Aşkın <askinbolat@gmail.com>
2022-10-15 00:19:25 +0300

    algoritma soru 3

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace algoritma3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir adet pozitif sayı girin.");
+            int sayi = int.Parse(Console.ReadLine());
+
+            
+            string[] kelimeler = new string[sayi];
+
+            for (int i = 0; i < sayi; i++)
+            {
+                Console.WriteLine("{0}. kelimeyi giriniz",i+1);
+                kelimeler[i] = Console.ReadLine();
+            }
+
+            Array.Reverse(kelimeler);
+            foreach (var item in kelimeler)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
